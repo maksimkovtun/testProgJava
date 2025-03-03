41385ea8b4755018fbaaef6f66582c546781fa10
maksimkovtun <makskovtun2002@mail.ru>
2025-03-03 16:55:49 +0700

    Initial

---
+++ src/main/java/com/program/testProgJava/TestProgJavaApplication.java
@@ -0,0 +1,13 @@
+package com.program.testProgJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProgJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProgJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/program/testProgJava/TestProgJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.program.testProgJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProgJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
